95308305dac6ca699707afe6d49e887403695aab
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-12-19 13:49:13 -0800

    static assets

--- Week8/Day2/03-create-route-handlers/server.js
+++ Week8/Day2/03-create-route-handlers/server.js
@@ -1,4 +1,5 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
 let nextDogId = 1;
 
@@ -11,8 +12,9 @@ function getNewDogId() {
 const server = http.createServer((req, res) => {
   console.log(`${req.method} ${req.url}`);
 
-  let reqBody = "";
+  let reqBody = ""; // affiliate=nasa&query=mars+rover%21&commit=Search
   req.on("data", (data) => {
+    // affiliate=nasa&query=mars+rover%21&commit=Search
     reqBody += data;
   });
 
@@ -20,29 +22,81 @@ const server = http.createServer((req, res) => {
   req.on("end", () => {
     // Parsing the body of the request
     if (reqBody) {
+      // affiliate=nasa&query=mars+rover%21&commit=Search
       req.body = reqBody
-        .split("&")
-        .map((keyValuePair) => keyValuePair.split("="))
-        .map(([key, value]) => [key, value.replace(/\+/g, " ")])
-        .map(([key, value]) => [key, decodeURIComponent(value)])
+        .split("&") // [affiliate=nasa,query=mars+rover%21,commit=Search]
+        .map((keyValuePair) => keyValuePair.split("=")) // [[affiliate,nasa],[query,mars+rover%21],[commit,Search]]
+        .map(([key, value]) => [key, value.replace(/\+/g, " ")]) // [[affiliate,nasa],[query,mars rover%21],[commit,Search]]
+        .map(([key, value]) => [key, decodeURIComponent(value)]) // [[affiliate,nasa],[query,mars rover!],[commit,Search]]
         .reduce((acc, [key, value]) => {
           acc[key] = value;
           return acc;
-        }, {});
+        }, {}); /*
+            {
+              affiliate:nasa,
+              query:mars rover!,
+              commit:Search
+            }
+        */
       console.log(req.body);
     }
     // Do not edit above this line
+    if (req.method === "GET" && req.url === "/home") {
+      const responseBody = fs.readFileSync("index.html", "utf-8");
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/html");
+      return res.end(responseBody);
+    }
+
+    if (req.method === "GET" && req.url === "/banana/index.css") {
+      const responseBody = fs.readFileSync("./styles/index.css", "utf-8");
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/css");
+      return res.end(responseBody);
+    }
 
     // define route handlers here
+    if (req.method === "GET" && req.url === "/") {
+      const responseBody = "Dog Club";
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end(responseBody);
+    }
 
+    // /dogs/:dogId , /dogs/:dogId/comments
+    if (req.method === "GET" && req.url === "/dogs") {
+      const responseBody = "Dog Index";
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end(responseBody);
+    }
+
+    if (req.method === "GET" && req.url.startsWith("/dogs")) {
+      console.log("req url ", req.url);
+      const urlParts = req.url.split("/");
+      console.log("url parts ", urlParts);
+      const dogId = urlParts[2];
+
+      const responseBody = `Dog details for dogId: ${dogId}`;
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end(responseBody);
+    }
+
+    if (req.method === "POST" && req.url === "/dogs") {
+      const dogId = getNewDogId();
+      res.statusCode = 302;
+      res.setHeader("Location", `/dogs/${dogId}`); // /dogs/:dogId
+      return res.end();
+    }
     // Do not edit below this line
     // Return a 404 response when there is no matching route handler
     res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    return res.end('No matching route handler found for this endpoint');
+    res.setHeader("Content-Type", "text/plain");
+    return res.end("No matching route handler found for this endpoint");
   });
 });
 
 const port = 5000;
 
-server.listen(port, () => console.log('Server is listening on port', port));
+server.listen(port, () => console.log("Server is listening on port", port));
--- Week8/Day2/04-serve-static-assets/server.js
+++ Week8/Day2/04-serve-static-assets/server.js
@@ -3,6 +3,21 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Your code here
+  // BONUS: combine route handler for images and css into one route handler
+
+
+  // route handler for images
+
+  // route handler for css
+
+  // route handler for home page (GET && '/')
+  // get response body (fs.readFileSync(path to html file, utf-8))
+
+  // set status code
+
+  // set header
+
+  // return response body
 });
 
 const port = 5000;
