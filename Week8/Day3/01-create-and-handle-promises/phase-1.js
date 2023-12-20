789ac44c55e296030b46fbf2e80a9366ffb0fa67
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-12-20 10:31:27 -0800

    promises solution

--- Week8/Day3/01-create-and-handle-promises/phase-1.js
+++ Week8/Day3/01-create-and-handle-promises/phase-1.js
@@ -1,19 +1,46 @@
 function stretch() {
   // Your code here
+  return new Promise((resolve, reject) => {
+    reject("Not going to stretch");
+    setTimeout(() => {
+      // console.log("done stretching");
+      resolve("done stretching");
+    }, 1000);
+  });
 }
 
 function runOnTreadmill() {
   // Your code here
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // console.log("done running on treadmill");
+      resolve("done running on treadmill");
+    }, 500);
+  });
 }
 
 function liftWeights() {
   // Your code here
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      // console.log("done lifting weights");
+      resolve("done lifting weights");
+    }, 2000);
+  });
 }
 
 function workout() {
   // Your code here
+  // stretch()
+  //   .then(() => runOnTreadmill())
+  //   .then(() => liftWeights())
+  //   .then(() => console.log("done working out"));
+  Promise.all([stretch(), runOnTreadmill(), liftWeights()])
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((err) => console.log(err));
 }
-
 
 /* ============================ TEST YOUR CODE ============================
 
@@ -21,10 +48,9 @@ Run the file (`node phase-1.js`) and check your output against the expected
 output.
 */
 
-
 workout();
-  // should print out the following:
-    // done stretching
-    // done running on treadmill
-    // done lifting weights
-    // done working out
+// should print out the following:
+// done stretching
+// done running on treadmill
+// done lifting weights
+// done working out
