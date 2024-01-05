7a1d7b0e1293ab70eb4296aa5bd12b306ba51be8
gootieno <gotieno@geoffreys-MacBook-Pro.local>
2024-01-05 13:31:27 -0800

    sp solutions

--- Week9/Day4/03-catsagram-long-practice-day-3/public/main.js
+++ Week9/Day4/03-catsagram-long-practice-day-3/public/main.js
@@ -1,52 +1,57 @@
-import { resetScore } from './score.js';
-import { resetComments } from './comments.js';
+import { resetScore } from "./score.js";
+import { resetComments } from "./comments.js";
 
-export const createMainContent = () => {
-    // Create h1
-    const h1 = document.createElement("h1");
-    h1.innerText = "Catstagram";
+export const createMainContent = async () => {
+  // Create h1
+  const h1 = document.createElement("h1");
+  h1.innerText = "Catstagram";
 
-    // Create img
-    const img = document.createElement("img");
-    img.style.margin = "20px";
-    img.style.maxWidth = "750px";
+  // Create img
+  const img = document.createElement("img");
+  img.id = "kitten-image";
+  img.style.margin = "20px";
+  img.style.maxWidth = "750px";
 
-    const newKittenBtn = createNewKittenBtn();
+  const newKittenBtn = createNewKittenBtn();
 
-    const container = document.querySelector(".container");
-    container.appendChild(h1);
-    container.append(newKittenBtn);
-    container.appendChild(img);
+  const container = document.querySelector(".container");
+  container.appendChild(h1);
+  container.append(newKittenBtn);
+  container.appendChild(img);
 
-    fetchImage();
+  let kittenImgUrl = localStorage.getItem('kittenImgUrl')
+  if(kittenImgUrl) img.src = kittenImgUrl
+  else img.src = await fetchImage();
 };
 
 const fetchImage = async () => {
-    // Fetch image from API and set img url
-    try {
-        const kittenResponse = await fetch("https://api.thecatapi.com/v1/images/search?size=small");
-        // Converts to JSON
-        const kittenData = await kittenResponse.json();
-        // console.log(kittenData);
-        const kittenImgUrl = kittenData[0].url;
-        const kittenImg = document.querySelector("img");
-        kittenImg.src = kittenImgUrl;
-
-        // After the image is finished loading, reset the score and comments
-        kittenImg.addEventListener('load', () => {
-            resetScore();
-            resetComments();
-        });
-    } catch (e) {
-        console.log("Failed to fetch image", e);
-    }
+  // Fetch image from API and set img url
+  try {
+    const kittenResponse = await fetch(
+      "https://api.thecatapi.com/v1/images/search?size=small"
+    );
+    // Converts to JSON
+    const kittenData = await kittenResponse.json();
+    // console.log(kittenData);
+    const kittenImgUrl = kittenData[0].url;
+    localStorage.setItem("kittenImgUrl", kittenImgUrl);
+    return kittenImgUrl;
+  } catch (e) {
+    console.log("Failed to fetch image", e);
+  }
 };
 
 const createNewKittenBtn = () => {
-    // Create "New Kitten" button
-    const newKittenBtn = document.createElement("button");
-    newKittenBtn.id = "new-kitten";
-    newKittenBtn.innerText = "New Kitten";
-    newKittenBtn.addEventListener('click', fetchImage);
-    return newKittenBtn;
+  // Create "New Kitten" button
+  const newKittenBtn = document.createElement("button");
+  newKittenBtn.id = "new-kitten";
+  newKittenBtn.innerText = "New Kitten";
+  newKittenBtn.addEventListener("click", async () => {
+    const kittenImgUrl = await fetchImage();
+    console.log("kitten img url ", kittenImgUrl);
+
+    const kittenImg = document.querySelector("img");
+    kittenImg.src = kittenImgUrl;
+  });
+  return newKittenBtn;
 };
